24213ccd52afebc87ceae4c34375d117a8673b55
Marley Kanui <marley.kanui@gmail.com>
2019-12-31 20:02:53 +0000

    EBDP 1.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'ezgi-bday-2020.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
